2a8d669851740d295f5d43aeeb26621bfc0b21c1
Roman Shevchenko <sirromas@gmail.com>
2016-03-29 14:55:46 +0200

    Add new logo

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -68,7 +68,8 @@
 
         <div id="wrapper">
             <header id="page-header" class="clearfix">
-                <div class="container-fluid"><div class="text-center"><a href="http://cnausa.com/"><img src="http://cnausa.com/assets/logo/logo_site.jpg" width="300" height="90"/></a></div></div>
+                <!-- <div class="container-fluid"><div class="text-center"><a href="http://cnausa.com/"><img src="http://cnausa.com/assets/logo/logo_site.jpg" width="300" height="90"/></a></div></div> -->
+                <div class="container-fluid"><div class="text-center"><a href="http://cnausa.com/"><img src="http://cnausa.com/assets/logo/5.jpg" width="300" height="90"/></a></div></div>
             </header>
 
             <header  class="navbar">
