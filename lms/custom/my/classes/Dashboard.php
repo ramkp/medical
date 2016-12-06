d8e6ff4d04f23a5b9f0179c4d15494d46bfd6e91
sirromas <sirromas@gmail.com>
2016-12-06 20:26:27 +0200

    Fix survey draw blood issue

--- lms/custom/my/classes/Dashboard.php
+++ lms/custom/my/classes/Dashboard.php
@@ -1825,10 +1825,18 @@ class Dashboard extends Util {
         $list = "";
         $list.="<select id='s_$input_id' style='width:220px;'>";
         $list.="<option value='--' selected>Please select</option>";
-        $list.="<option value='1'>Excellent</option>";
-        $list.="<option value='2'>Good</option>";
-        $list.="<option value='3'>Needs improvement</option>";
-        $list.="<option value='4'>Very disappointing</option>";
+        if ($input_id != 'draw_blood') {
+            $list.="<option value='1'>Excellent</option>";
+            $list.="<option value='2'>Good</option>";
+            $list.="<option value='3'>Needs improvement</option>";
+            $list.="<option value='4'>Very disappointing</option>";
+        } // end if
+        else {
+            $list.="<option value='1'>1</option>";
+            $list.="<option value='2'>2</option>";
+            $list.="<option value='3'>3</option>";
+            $list.="<option value='4'>4</option>";
+        } // end else
         $list.="</select>";
 
         return $list;
