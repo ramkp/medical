f7aa3ac2e5edda15287d7cf11b389b39ca3f805a
sirromas <sirromas@gmail.com>
2017-06-14 08:55:34 +0300

    Trim search item data

--- lms/custom/users/search_user.php
+++ lms/custom/users/search_user.php
@@ -3,8 +3,7 @@
 require_once './classes/User.php';
 $user = new User();
 $email = $_POST['email'];
-$list = $user->search_user_by_email($email);
+$list = $user->search_user_by_email(trim($email));
 echo $list;
-
 ?>
 
