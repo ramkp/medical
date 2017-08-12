c2e14f366a5f5212c2fb61b2eac1c5609f97ff4b
sirromas <sirromas@gmail.com>
2017-08-12 06:48:20 +0300

    Print grade report for each course separately

--- lms/custom/grades/classes/Grades.php
+++ lms/custom/grades/classes/Grades.php
@@ -38,7 +38,7 @@ class Grades extends Util {
         $query = "select * from mdl_grade_grades "
                 . "where itemid=$item "
                 . "and userid=$userid "
-                . "and finalgrade is not null";
+                . "and finalgrade is not null ";
         $num = $this->db->numrows($query);
         if ($num > 0) {
             $result = $this->db->query($query);
@@ -46,7 +46,12 @@ class Grades extends Util {
                 $pr = new stdClass();
                 $name = $this->get_quiz_item_name($item);
                 $date = date('m-d-Y', $row['timemodified']);
-                $grade = round($row['finalgrade']);
+                if ($row['finalgrade'] < 100) {
+                    $grade = round($row['finalgrade']);
+                } // end if 
+                else {
+                    $grade = round(($row['finalgrade'] / $row['rawgrademax']) * 100);
+                } // end else 
                 $pr->id = $item;
                 $pr->name = $name;
                 $pr->grade = $grade;
@@ -136,20 +141,28 @@ class Grades extends Util {
         return $courseid;
     }
 
-    function create_pdf_report($userid) {
+    function create_user_separate_grade_reports($refid, $courseid, $userid) {
         $list = "";
+        $file = '';
         $user = $this->get_user_details($userid);
-        $courses = $this->get_user_courses($userid);
-        $total_grade = 0;
-        $total_grade_max = 0;
-        //$list.="<p align='center' style='font-weight:bold;'>$user->firstname $user->lastname</p>";
-        if (count($courses) > 0) {
-            $list.="<table align='center'>";
-            
-            $list.="<tr>";
-            
-            $list.="<td colspan='3'>";
-            
+        $grades = $this->get_student_grades($courseid, $userid);
+        $coursename = $this->get_course_name($courseid);
+
+        if (count($grades) > 0) {
+            $list.="<html>";
+
+            $list.="<head>";
+
+            $list.="<style>";
+
+            $list.="</style>";
+
+            $list.="</head>";
+
+            $list.="<body>";
+
+
+
             $list.="<table align='center' border='0' width='100%'>
 
                     <tr>
@@ -207,66 +220,220 @@ class Grades extends Util {
                         </tr>
 
                      </table>";
-            
-            $list.="</td>";
-            
-            $list.="</tr>";
-            
+
+
+            $list.="<p style='text-align:center;'>$user->firstname $user->lastname</p>";
+
+
+            $list.="<table align='center'>";
             $list.="<tr>";
-            
-            $list.="<td colspan='3' style='font-weight:bold;font-size:18px;text-align:center;'>";
-            
-            $list.="$user->firstname $user->lastname";
-            
-            $list.="</td>";
-            
+            $list.="<th colspan='3'>$coursename</th>";
             $list.="</tr>";
-            
-            foreach ($courses as $courseid) {
-                $grades = $this->get_student_grades($courseid, $userid);
-                $coursename = $this->get_course_name($courseid);
+            $total_grade = 0;
+            $total_grade_max = 0;
+
+            foreach ($grades as $gr) {
+                $total_grade = $total_grade + $gr->grade;
+                $total_grade_max = $total_grade_max + $gr->max;
                 $list.="<tr>";
-                $list.="<th colspan='3'>$coursename</th>";
+                $list.="<td style='padding:15px;'>$gr->name</td>";
+                $list.="<td style='padding:15px;'>$gr->grade%</td>";
+                $list.="<td style='padding:15px;'>$gr->date</td>";
                 $list.="</tr>";
-                if (count($grades) > 0) {
-                    foreach ($grades as $gr) {
-                        $total_grade = $total_grade + $gr->grade;
-                        $total_grade_max = $total_grade_max + $gr->max;
-                        $list.="<tr>";
-                        $list.="<td style='padding:15px;'>$gr->name</td>";
-                        $list.="<td style='padding:15px;'>$gr->grade%</td>";
-                        $list.="<td style='padding:15px;'>$gr->date</td>";
-                        $list.="</tr>";
-                    } // end foreach
-                    $average = round(($total_grade / $total_grade_max) * 100);
-                    $list.="<tr>";
-                    $list.="<td style='padding:15px;' colspan='3'><hr/></td>";
-                    $list.="</tr>";
-                    $list.="<tr>";
-                    $list.="<td style='padding:15px;'>Total grade $total_grade points</td>";
-                    $list.="</tr>";
-                    $list.="<tr>";
-                    $list.="<td style='padding:15px;'>Total course max $total_grade_max points</td>";
-                    $list.="</tr>";
-                    $list.="<tr>";
-                    $list.="<td style='padding:15px;'>Average $average %</td>";
-                    $list.="</tr>";
-                } // end if count($grades)>0
             } // end foreach
+
+            $average = round(($total_grade / $total_grade_max) * 100);
+            $list.="<tr>";
+            $list.="<td style='padding:15px;' colspan='3'><hr/></td>";
+            $list.="</tr>";
+            $list.="<tr>";
+            $list.="<td style='padding:15px;'>Total grade $total_grade points</td>";
+            $list.="</tr>";
+            $list.="<tr>";
+            $list.="<td style='padding:15px;'>Total course max $total_grade_max points</td>";
+            $list.="</tr>";
+            $list.="<tr>";
+            $list.="<td style='padding:15px;'>Average $average %</td>";
+            $list.="</tr>";
             $list.="</table>";
-        } // end if count($courses)>0
-        $dir = $this->report_dir . "/$userid";
-        if (!is_dir($dir)) {
-            if (!mkdir($dir)) {
-                die('Could not write to disk');
-            } // end if !mkdir($dir_path)
-        }
-        $file = "grades_report.pdf";
-        $path = $dir . "/$file";
-        $pdf = new mPDF('utf-8', 'A4-P');
-        $pdf->WriteHTML($list);
-        $pdf->Output($path, 'F');
+
+            $list.="</body>";
+
+            $list.="</html>";
+
+            $dir = $this->report_dir . "/$userid";
+            if (!is_dir($dir)) {
+                if (!mkdir($dir)) {
+                    die('Could not write to disk');
+                } // end if !mkdir($dir_path)
+            }
+            $file = "grades_report_$refid.pdf";
+            $path = $dir . "/$file";
+            $pdf = new mPDF('utf-8', 'A4-P');
+            $pdf->WriteHTML($list);
+            $pdf->Output($path, 'F');
+        } // end if count of grades
+
         return $file;
+    }
+
+    function create_pdf_report($userid) {
+        $courses = $this->get_user_courses($userid);
+
+        if (count($courses) > 0) {
+            /*
+              $list.="<html>";
+
+              $list.="<head>";
+
+              $list.="<style>";
+
+              $list.="</style>";
+
+              $list.="</head>";
+
+              $list.="<body>";
+
+              $list.="<table align='center'>";
+
+              $list.="<tr>";
+
+              $list.="<td colspan='3'>";
+
+              $list.="<table align='center' border='0' width='100%'>
+
+              <tr>
+
+              <td style='padding-top:10px;'><img src='https://medical2.com/assets/icons/logo3.png' width='115' height='105'></td>
+
+              <td valign='top'>
+
+              <table style='padding:15px;font-size:12px;' align='left'>
+
+              <tr>
+              <td style='font-size:20px;font-weight:bold;letter-spacing:8px;padding-left:65px;'>Medical2</td>
+              </tr>
+
+              <tr>
+              <td style='font-size:15px;font-weight:bold;letter-spacing:6px;padding-left:40px;'>Career College</td>
+              </tr>
+
+              <tr>
+              <td style='padding-top:10px;padding-left:75px;'>1830A North Gloster St</td>
+              </tr>
+
+              <tr>
+              <td style='padding-left:90px;'>Tupelo, MS 38804</td>
+              </tr>
+
+              </table>
+
+              </td>
+
+              <td align='right' valign='top'>
+
+              <table style='padding:15px;font-size:12px;'>
+
+              <tr>
+              <td style='padding-top:5px;'>Phone: 877-741-1996</td>
+              </tr>
+
+              <tr>
+              <td>Fax: 1-407-233-1192</td>
+              </tr>
+
+              <tr>
+              <td style='padding-top:25px;'>Email: info@medical2.com</td>
+              </tr>
+
+              <tr>
+              <td>Web: www.medical2.com</td>
+              </tr>
+
+              </table>
+
+              </td>
+
+              </tr>
+
+              </table>";
+
+              $list.="</td>";
+
+              $list.="</tr>";
+
+              $list.="<tr>";
+
+              $list.="<td colspan='3' style='font-weight:bold;font-size:18px;text-align:center;'>";
+
+              $list.="$user->firstname $user->lastname";
+
+              $list.="</td>";
+
+              $list.="</tr>";
+
+              foreach ($courses as $courseid) {
+              $grades = $this->get_student_grades($courseid, $userid);
+              $coursename = $this->get_course_name($courseid);
+              $list.="<tr>";
+              $list.="<th colspan='3'>$coursename</th>";
+              $list.="</tr>";
+              if (count($grades) > 0) {
+              foreach ($grades as $gr) {
+              $total_grade = $total_grade + $gr->grade;
+              $total_grade_max = $total_grade_max + $gr->max;
+              $list.="<tr>";
+              $list.="<td style='padding:15px;'>$gr->name</td>";
+              $list.="<td style='padding:15px;'>$gr->grade%</td>";
+              $list.="<td style='padding:15px;'>$gr->date</td>";
+              $list.="</tr>";
+              } // end foreach
+              $average = round(($total_grade / $total_grade_max) * 100);
+              $list.="<tr>";
+              $list.="<td style='padding:15px;' colspan='3'><hr/></td>";
+              $list.="</tr>";
+              $list.="<tr>";
+              $list.="<td style='padding:15px;'>Total grade $total_grade points</td>";
+              $list.="</tr>";
+              $list.="<tr>";
+              $list.="<td style='padding:15px;'>Total course max $total_grade_max points</td>";
+              $list.="</tr>";
+              $list.="<tr>";
+              $list.="<td style='padding:15px;'>Average $average %</td>";
+              $list.="</tr>";
+              } // end if count($grades)>0
+              $list.="<p style='page-break-after: always;'>&nbsp;</p>";
+
+              }// end foreach courses
+
+
+              $list.="</table>";
+
+              $list.="</body>";
+
+              $list.="</html>";
+             * 
+             */
+
+            $flash = time() . $userid;
+            $i = 0;
+            foreach ($courses as $courseid) {
+                $refID = $flash . $i;
+                $file = $this->create_user_separate_grade_reports($refID, $courseid, $userid);
+                if ($file != '') {
+                    $files[] = $_SERVER['DOCUMENT_ROOT'] . "/lms/custom/grades/$userid/" . $file;
+                }
+                $i++;
+            } // end foreach
+        } // end if count($courses)>0
+
+        $outputName = $this->report_dir . "/$userid/" . $flash . "_merged.pdf";
+        $cmd = "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$outputName ";
+        foreach ($files as $file) {
+            $cmd .= $file . " ";
+        } // end foreach
+        shell_exec($cmd);
+        return $flash . "_merged.pdf";
     }
 
 }
